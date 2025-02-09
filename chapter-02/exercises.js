fb6b4ed1af5a1bf65bbced843276482569174f3b
TeyPeyPey <teyonce143@gmail.com>
2025-02-09 13:54:19 -0600

    saving work

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -55,9 +55,19 @@ on the number:
 */
 
 function fizzBuzz(start, end) {
-  
+  for (var i = start; i <= end; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0){
+      console.log("fizz");
+    } else if (i % 5 === 0){
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
-
+console.log(fizzBuzz(1, 15));
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
